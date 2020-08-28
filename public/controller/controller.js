104968b4bcb4ba9fc1a4fe94bada30fc82364f05
Fatma Usalan <b21727829@cs.hacettepe.edu.tr>
2020-08-28 10:30:30 +0300

    some correction

--- public/controller/controller.js
+++ public/controller/controller.js
@@ -1,6 +1,9 @@
 angular.module('HelloBlog', [])
 .controller('AppController', function($scope, $http){
-    var globalImg;
+    $scope.addButton = function(){
+        console.log($scope.test);
+    }
+   var globalImg;
     var refresh = function(){
         $http.get('/writingList').then(function(response){
             $scope.writingList = response.data;
@@ -14,7 +17,7 @@ angular.module('HelloBlog', [])
             var reader = new FileReader();
             reader.onload = function(e){
                 document.getElementById("blah").src = e.target.result
-                globalImg = e.target.result
+                $scope.newW.imageSrc = e.target.result
             };
            reader.readAsDataURL(input.files[0]);
            // console.log(input.files[0]);
@@ -22,14 +25,14 @@ angular.module('HelloBlog', [])
     }
     
    $scope.addWriting = function(){
-        $scope.newW.imageSrc = globalImg;
+        //$scope.newW.imageSrc = globalImg; // burayı dene unutma 
         console.log($scope.newW);
 
         $http.post('/writingList',$scope.newW).then(function(response){
             //console.log(response.data);
-           // refresh();
+            refresh();
         });
-   }
+   }  
   
 })
 
